d5f2f8d01a70e4e078eabd375f193b8829bf77a0
Grigory Kalabin <grigoriy.kalabin@lazada.com>
2015-10-01 15:38:23 +0300

    Remove regex flag property from user agent since specification says that only device can have it

--- uaparser/user_agent.go
+++ uaparser/user_agent.go
@@ -14,7 +14,6 @@ type UserAgent struct {
 type UserAgentPattern struct {
 	Regexp            *regexp.Regexp
 	Regex             string
-	RegexFlag         string
 	FamilyReplacement string
 	V1Replacement     string
 	V2Replacement     string
